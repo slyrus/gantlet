3045e73ae97426812504c2b397931a6d8d286cc1
Cyrus Harmon <cyrus@bobobeach.com>
2018-11-27 11:01:56 -0800

    try some new colors

--- presentation.lisp
+++ presentation.lisp
@@ -13,16 +13,15 @@
         (make-rgb-color 0.5 0.5 0.75)))
 
 (defparameter *task-border-colors*
-  (list (make-rgb-color 0.6 0.6 0.6)
+  (list (make-rgb-color 0.6 0.5 0.6)
         (make-rgb-color 0.5 0.5 0.5)))
 
 (defparameter *task-background-colors*
-  (list (make-rgb-color 0.93 0.93 0.93)
-        (make-rgb-color 0.96 0.96 0.96)))
+  (list (make-rgb-color 0.03 0.03 0.03)
+        (make-rgb-color 0.06 0.06 0.06)))
 
 (defparameter *critical-task-color*
-  nil
-  #+(or) (make-rgb-color 1 0.5 0.5))
+  (make-rgb-color 1 0.0 0.0))
 
 (defparameter *complete-task-color*
   (make-rgb-color 0.5 0.5 1))
@@ -39,6 +38,8 @@
      (+ b (* (- 1 b) lightness)))))
 
 (defparameter *task-colors*
+  (list (make-rgb-color 0.0 0.4 0.0))
+  #+nil
   (mapcar (lambda (color)
             (lighten-color color 0.6))
           *task-border-colors*))
@@ -159,6 +160,9 @@
               (let ((text-x-offset (if (> max-width (- x2 x1 7))
                                        (+ x2 text-left-margin 2)
                                        (+ x1 text-left-margin)))
+                    (text-color (if (> max-width (- x2 x1 7))
+                                    +white+
+                                    +white+))
                     (y-offset y1))
                 (flet ((draw-text-line (text-spec)
                          (destructuring-bind (text width height)
@@ -171,7 +175,7 @@
                                            (+ y-offset text-top-margin)
                                            (+ y-offset (/ (- y2 y1) 2)))
                                        :align-y (if expanded :top :center)
-                                       :ink +black+
+                                       :ink text-color
                                        :text-style style)
                            (incf y-offset height))))
                   (let ((lines (reverse text-list)))
@@ -209,7 +213,7 @@
            (pane-width (rectangle-width (sheet-region pane)))
            (pane-unit (/ pane-task-length pane-width))
            (task-height 6)
-           (task-padding 1)
+           (task-padding 2)
            (bottom-margin 1)
            (x-zoom (zoom-x-level task-view))
            (y-zoom (zoom-y-level task-view))
